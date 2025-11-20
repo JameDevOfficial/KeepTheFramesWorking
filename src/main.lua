b619c75f2061ebf9eb45e2f41486ce52c387890e
jamedevofficial <jame.bayerl@gmai.com>
2025-11-20 20:22:47 +0100

    feat: add basic files and project structure

---
+++ src/main.lua
@@ -0,0 +1,23 @@
+
+World = love.physics.newWorld(0, 0, true)
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key, scancode, isrepeat)
+
+end
+
+function love.keyreleased(key, scancode)
+
+end
+
